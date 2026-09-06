bb076c71aa282df1b2346cbc56d7a73dc6646efd
Prashant Kumar <kumarprashant1345@gmail.com>
2026-09-06 11:50:34 +0530

    added routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,21 @@
-
-
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Body from "./components/Body"
-
+import Login from "./components/Login"
+import Profile from "./components/Profile"
 
 function App() {
 
 
   return (
     <>
-
-      <Body />
-
+      <BrowserRouter basename="/" >
+        <Routes>
+          <Route path="/" element={<Body/>} > 
+            <Route path="/login" element={<Login/>}/>
+            <Route path="/profile" element={<Profile/>}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
