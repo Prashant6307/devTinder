9e59f5341d773196e5487620b18f8014ecd52212
Prashant Kumar <kumarprashant1345@gmail.com>
2026-09-06 12:45:27 +0530

    folder restructured

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -54,6 +54,12 @@ userRouter.get("/feed", userAuth, async (req, res) => {
     try {
         const loggedInUser = req.body
 
+        const page = parseInt(req.query.page) || 1
+        let limit = parseInt(req.query.limit) || 10
+        limit = limit >50 ? 50 : limit
+
+        const skip = (page-1) * limit
+
         const connectionRequests = await ConnectionRequestModel.find({
             $or: [
                 { fromUserId: loggedInUser._id },
@@ -72,7 +78,7 @@ userRouter.get("/feed", userAuth, async (req, res) => {
                 { _id: { $nin: Array.from(hideUsersFromFeed) } },
                 { _id: { $ne: loggedInUser._id } }
             ]
-        }).select(USER_SAFE_DATA)
+        }).select(USER_SAFE_DATA).skip(skip).limit(limit)
 
         res.send(users)
 
--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,11 +1,57 @@
-
+import axios from "axios"
+import { useState } from "react"
 
 const Login = () => {
-  return (
-    <div>
-      login
-    </div>
-  )
+
+    const [emailId, setEmailId] = useState("")
+    const [password, setPassword] = useState("")
+
+
+    const handleLogin = async () => {
+        try {
+            const res = await axios.post("http://localhost:3000/login", {
+                emailId,
+                password
+            })
+        }
+        catch (err) {
+            console.log(err.message)
+        }
+    }
+
+    return (
+        <div className="hero bg-base-200 min-h-screen">
+            <div className="hero-content flex-col sm:w-96">
+                <div className="text-center ">
+                    <h1 className="text-5xl font-bold">Login now!</h1>
+                </div>
+                <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                    <div className="card-body">
+                        <fieldset className="fieldset">
+                            <label className="label">Email</label>
+                            <input
+                                type="email"
+                                className="input"
+                                placeholder="Email"
+                                value={emailId}
+                                onChange={(e) => setEmailId(e.target.value)}
+                            />
+                            <label className="label">Password</label>
+                            <input
+                                type="password"
+                                className="input"
+                                placeholder="Password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                            <div><a className="link link-hover">Forgot password?</a></div>
+                            <button className="btn btn-neutral mt-4" onClick={handleLogin}>Login</button>
+                        </fieldset>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Login
