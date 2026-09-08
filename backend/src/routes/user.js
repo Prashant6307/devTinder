aeb7d765bc02251747e4e297ca401fc929d55675
Prashant Kumar <kumarprashant1345@gmail.com>
2026-09-08 14:16:53 +0530

    created user feed

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -52,7 +52,7 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
 
 userRouter.get("/feed", userAuth, async (req, res) => {
     try {
-        const loggedInUser = req.body
+        const loggedInUser = req.user
 
         const page = parseInt(req.query.page) || 1
         let limit = parseInt(req.query.limit) || 10
--- frontend/src/components/Feed.jsx
+++ frontend/src/components/Feed.jsx
@@ -1,8 +1,35 @@
+import axios from "axios"
+import { BASE_URL } from "../utils/constant"
+import { useDispatch, useSelector } from "react-redux"
+import { addFeed } from "../utils/feedSlice"
+import { useEffect } from "react"
+import UserCard from "./UserCard"
 
 const Feed = () => {
+  const feed = useSelector(store => store.feed)
+  const dispatch = useDispatch()
+
+  const getFeed = async () => {
+    try {
+      if (feed) return
+      const res = await axios.get(BASE_URL + "/feed", {
+        withCredentials: true
+      })
+      dispatch(addFeed(res.data))
+    } catch (err) {
+      console.log(err.message)
+    }
+  }
+
+  useEffect(() => {
+    getFeed()
+  }, [])
+
+
+
   return (
-    <div>
-      
+    <div className="flex justify-center items-center my-10 p-10">
+      <UserCard />
     </div>
   )
 }
