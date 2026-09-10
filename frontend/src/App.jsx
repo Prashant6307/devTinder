706597145f0b5c9ad4eb8c07a5ee9c3b08c48865
Prashant Kumar <kumarprashant1345@gmail.com>
2026-09-10 11:14:24 +0530

    built connections page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Profile from "./components/Profile"
 import appStore from "./utils/appStore"
 import { Provider } from "react-redux"
 import Feed from "./components/Feed"
+import Connections from "./components/Connections"
 
 function App() {
 
@@ -18,6 +19,7 @@ function App() {
             <Route path="/" element={<Feed />}/>
               <Route path="/login" element={<Login />} />
               <Route path="/profile" element={<Profile />} />
+              <Route path="/connections" element={<Connections />} />
             </Route>
           </Routes>
         </BrowserRouter>
@@ -25,5 +27,6 @@ function App() {
     </>
   )
 }
+
 
 export default App
---
+++ frontend/src/components/Connections.jsx
@@ -0,0 +1,55 @@
+import axios from "axios"
+import { BASE_URL } from "../utils/constant"
+import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { addConnections } from "../utils/connectionsSlice"
+
+const Connections = () => {
+  const connections = useSelector(store => store.connections)
+  const dispatch = useDispatch()
+
+  const fetchConnections = async () => {
+    try {
+      const res = await axios.get(BASE_URL + "/user/connections", {
+        withCredentials: true
+      })
+      console.log(res)
+      dispatch(addConnections(res.data.data))
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
+
+  useEffect(() => {
+    fetchConnections()
+  }, [])
+
+  if (!connections) return <h1>No Connections Found</h1>
+
+  if (connections.length === 0) {
+    return <h1>No Connections Found</h1>
+  }
+
+  return (
+    <div className="text-center my-10">
+      <h1>Connections</h1>
+
+      {connections.map((connection) => {
+        const { firstName, lastName, photoUrl, age, gender, about, skills } = connection
+        return <div className="m-4 p-4 rounded-lg bg-base-200">
+          <div>
+            <img src={photoUrl} alt="photo" className="w-20 h-20" />
+          </div>
+          <div className="text-left mx-4">
+            <h2>{firstName + " " + lastName}</h2>
+            <p>{about}</p>
+          </div>
+
+        </div>
+      })}
+
+    </div>
+  )
+}
+
+export default Connections
